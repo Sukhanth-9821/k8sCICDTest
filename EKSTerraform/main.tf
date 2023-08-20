7a266fa9f59dcb242db1bc17b21924950727a00f
Sukhanth <sukhanthsukh1234h@gmail.com>
2023-08-20 16:54:44 +0530

    4

--- EKSTerraform/main.tf
+++ EKSTerraform/main.tf
@@ -21,7 +21,7 @@ data "aws_subnet" "subnet2"{
 module "eks" {
     source  = "terraform-aws-modules/eks/aws"
     version = "~> 19.0"
-    cluster_name = "suki-tf-cluster"
+    cluster_name = "suki-tf-cluster2"
     cluster_version = "1.24"
 
     cluster_endpoint_public_access  = true
